de673304b1d340101e12610023e0177a50654750
lucastoll <lucasfernandestolotto@gmail.com>
2022-03-07 21:46:36 -0300

    section home completa

---
+++ assets/js/anima-logo.js
@@ -0,0 +1,10 @@
+var looper;
+var graus=20;
+
+function rotate(){
+  document.querySelector("[data-divlogo-iconloop]").style.transform = `rotate(${graus}deg)`
+  graus++;
+  looper = setTimeout(rotate, 20);
+}
+
+setTimeout(rotate, 180);
