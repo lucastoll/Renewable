f9859b0d73033163735116dff2950f25fc768d53
lucastoll <lucasfernandestolotto@gmail.com>
2022-03-16 20:46:12 -0300

    Backbutton
    Feature backbutton (botão de voltar ao topo) incrementada

--- assets/js/anima-scroll.js
+++ assets/js/anima-scroll.js
@@ -1,4 +1,5 @@
 const sections = [...document.querySelectorAll("section")];
+let backbutton = document.querySelector("[data-backbutton]")
 
 let options = {
   rootMargin: "0px",
@@ -12,6 +13,19 @@ const { target } = entry;
 
 if (entry.intersectionRatio >= 0.25) {
 target.classList.add("is-visible");
+
+if(target.classList.contains("home"))
+{
+  backbutton.style.animation = "backbuttonLeave 0.5s forwards"
+}
+else
+{
+  backbutton.style.animation = "backbuttonEnter 0.5s forwards"
+}
+
+
+
+
 }
   });
 };
@@ -23,3 +37,47 @@ sections.forEach((section, index) => {
 });
 
 /* window.onload(removeClass()); */
+
+/* const animaBackbutton = function scrollOpacity () {
+  backbutton.style.opacity = "0.50"
+  backbutton.style.opacity = "0.50"
+  backbutton.style.opacity = "0.50"
+  backbutton.style.opacity = "0.50"
+  backbutton.style.opacity = "0.50"
+  backbutton.style.opacity = "0.50"
+  backbutton.style.opacity = "0.50"
+  backbutton.style.opacity = "0.50"
+  backbutton.style.opacity = "0.50"
+  backbutton.style.opacity = "0.50"
+  backbutton.style.opacity = "0.50"
+  backbutton.style.opacity = "0.50"
+  backbutton.style.opacity = "0.50"
+  backbutton.style.opacity = "0.50"
+  backbutton.style.opacity = "0.50"
+  backbutton.style.opacity = "0.50"
+  backbutton.style.opacity = "0.50"
+  backbutton.style.opacity = "0.50"
+  backbutton.style.opacity = "0.50"
+  backbutton.style.opacity = "0.50"
+  backbutton.style.opacity = "1"
+
+}
+ */
+
+let didScroll = false;
+
+var main = document.getElementById("main")
+main.onscroll = () => didScroll = true;
+
+setInterval(() => {
+    if ( didScroll ) {
+        didScroll = false;
+        backbutton.style.opacity = "0.50"
+    }
+    else{
+      backbutton.style.opacity = "1"
+    }
+}, 100);
+
+
+/* main.addEventListener("scroll", animaBackbutton); */
