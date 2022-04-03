056dd95a53d3d8087c379ffc8574854eab96eb96
lucastoll <lucasfernandestolotto@gmail.com>
2022-04-03 19:15:29 -0300

    seção energia desktop completa

---
+++ assets/js/owlcarousel.js
@@ -0,0 +1,21 @@
+/* Owl carousel */
+/* https://owlcarousel2.github.io/OwlCarousel2/docs/api-options.html */
+
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+         items: 1,
+         center: true,
+         loop: true,
+         margin: 0,
+         dots: true,
+         autoplay: true,
+         autoplayTimeout: 25000,
+         nav: false,
+    });
+
+  });
+
+  /* Scroll reveal carrousel */
+
+  /* Animação caseBox */
