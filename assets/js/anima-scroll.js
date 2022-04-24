79bf103109e5928391d1fbd4dca2321df44e1822
lucastoll <lucasfernandestolotto@gmail.com>
2022-04-23 22:18:41 -0300

    feat: animação seção final.

--- assets/js/anima-scroll.js
+++ assets/js/anima-scroll.js
@@ -1,6 +1,10 @@
 const sections = [...document.querySelectorAll("section")];
 const navbar = document.querySelector(".navbar");
 const home = document.getElementById("home");
+const lottie = document.getElementById("svg");
+const final = document.querySelector(".final");
+
+
 
 let options = {
   rootMargin: "0px",
@@ -19,24 +23,42 @@ if(window.innerWidth > 1440){
   if(home.getBoundingClientRect().top > -1000)
   {
     navbar.style.padding = "0px 40px"
-  }
+  }  
   else if(target.classList.contains("energia"))
   {
     navbar.style.padding = "0px 17px"
-  }
-}
+  }  
+}  
+if(final.getBoundingClientRect().top < 1000)
+{
+  lottie.classList.remove("hide");
+  setTimeout(()=>{
+    animItem.goToAndPlay(0,true);
+  }, 1000)
+}  
+
   });
-};
+};  
 
 const observer = new IntersectionObserver(callback, options);
 
 sections.forEach((section, index) => {
   observer.observe(section);
-});
+});  
 
 
 
 var main = document.getElementById("main")
 
 
+
+const animItem = bodymovin.loadAnimation({
+  wrapper: lottie,
+  animType: 'svg',
+  loop: false,
+  autoplay: false,
+  path: 'https://assets2.lottiefiles.com/packages/lf20_fehtbnef.json'
+});
+
+animItem.setSpeed(0.5);
 
